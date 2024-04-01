2a83560e6de9a69397c354a5bc7a3a342373b486
Diego Adolfo González Flores <diegogonzalezflores@outlook.com>
2024-04-01 10:27:36 -0600

    Adding globalization

---
+++ src/OneCore.CategorEyes.Client/Extensions/WebAssemblyHostExtension.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.JSInterop;
+using System.Globalization;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace OneCore.CategorEyes.Client.Extensions
+{
+    public static class WebAssemblyHostExtension
+    {
+        public async static Task SetDefaultCulture(this WebAssemblyHost host)
+        {
+            var jsInterop = host.Services.GetRequiredService<IJSRuntime>();
+            string? result;
+
+            try
+            {
+                result = await jsInterop.InvokeAsync<string>("blazorCulture.get");
+            }
+            catch (Exception)
+            {
+                result = null;
+            }
+
+            CultureInfo culture;
+
+            if (result != null)
+                culture = new CultureInfo(result);
+            else
+                culture = new CultureInfo("en-US");
+
+            CultureInfo.DefaultThreadCurrentCulture = culture;
+            CultureInfo.DefaultThreadCurrentUICulture = culture;
+        }
+    }
+}
--- src/OneCore.CategorEyes.Client/Program.cs
+++ src/OneCore.CategorEyes.Client/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using OneCore.CategorEyes.Client;
+using OneCore.CategorEyes.Client.Extensions;
 using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -9,5 +10,10 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddRadzenComponents();
+builder.Services.AddLocalization();
+
+var host = builder.Build();
+
+await host.SetDefaultCulture();
 
 await builder.Build().RunAsync();
