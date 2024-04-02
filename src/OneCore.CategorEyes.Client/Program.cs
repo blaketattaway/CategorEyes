6fd7db6e854c83e66849214a1f7bf6a6992fc62d
Diego Adolfo González Flores <diegogonzalezflores@outlook.com>
2024-04-02 00:39:27 -0600

    Refactor using "ChatGPT"

--- src/OneCore.CategorEyes.Client/Program.cs
+++ src/OneCore.CategorEyes.Client/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using OneCore.CategorEyes.Client;
 using OneCore.CategorEyes.Client.Extensions;
+using OneCore.CategorEyes.Client.Services;
 using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -11,6 +12,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddRadzenComponents();
 builder.Services.AddLocalization();
+builder.Services.AddScoped<CultureService>();
 
 var host = builder.Build();
 
---
+++ src/OneCore.CategorEyes.Client/Services/CultureService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Globalization;
+
+namespace OneCore.CategorEyes.Client.Services
+{
+    public class CultureService
+    {
+        private readonly IJSRuntime _jsRuntime;
+        private readonly NavigationManager _navigationManager;
+
+        public CultureService(IJSRuntime jsRuntime, NavigationManager navigationManager)
+        {
+            _jsRuntime = jsRuntime;
+            _navigationManager = navigationManager;
+        }
+
+        /// <summary>
+        /// Sets the application's culture to the specified new culture and forces the application to reload.
+        /// </summary>
+        /// <param name="newCulture">The new culture to set for the application, of type <see cref="CultureInfo"/>.</param>
+        public void SetCulture(CultureInfo newCulture)
+        {
+            if (CultureInfo.CurrentCulture != newCulture)
+            {
+                var js = (IJSInProcessRuntime)_jsRuntime;
+                js.InvokeVoid("blazorCulture.set", newCulture.Name);
+
+                _navigationManager.NavigateTo(_navigationManager.Uri, forceLoad: true);
+            }
+        }
+    }
+}
