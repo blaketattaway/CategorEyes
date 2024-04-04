8510679affb1d3ea50a7f8836641fad0dbb6ab3f
Diego Adolfo González Flores <diegogonzalezflores@outlook.com>
2024-04-03 22:28:40 -0600

    Adding summaries

--- src/OneCore.CategorEyes.Client/Extensions/WebAssemblyHostExtension.cs
+++ src/OneCore.CategorEyes.Client/Extensions/WebAssemblyHostExtension.cs
@@ -8,6 +8,11 @@ namespace OneCore.CategorEyes.Client.Extensions
     {
         private const string DEFAULT_CULTURE = "en-US";
 
+        /// <summary>
+        /// Sets the default culture for the application based on a persisted value or falls back to a default if not set.
+        /// </summary>
+        /// <param name="host">The <see cref="WebAssemblyHost"/> instance to extend, providing access to application services.</param>
+        /// <returns>A task that represents the asynchronous operation of setting the application's default culture.</returns>
         public async static Task SetDefaultCulture(this WebAssemblyHost host)
         {
             var jsInterop = host.Services.GetRequiredService<IJSRuntime>();
@@ -20,6 +25,13 @@ namespace OneCore.CategorEyes.Client.Extensions
             CultureInfo.DefaultThreadCurrentUICulture = culture;
         }
 
+        /// <summary>
+        /// Safely invokes a JavaScript function and returns the result. If the invocation fails, returns a default value.
+        /// </summary>
+        /// <param name="jsInterop">The <see cref="IJSRuntime"/> instance used for JavaScript interop calls.</param>
+        /// <param name="method">The JavaScript function to invoke.</param>
+        /// <param name="defaultValue">The default value to return in case the JavaScript function invocation fails.</param>
+        /// <returns>A task that represents the asynchronous operation, containing the result of the JavaScript function invocation or the default value if the invocation fails.</returns>
         private static async Task<string> SafeInvokeAsync(IJSRuntime jsInterop, string method, string defaultValue)
         {
             try
--- src/OneCore.CategorEyes.Client/Services/LoadingService.cs
+++ src/OneCore.CategorEyes.Client/Services/LoadingService.cs
@@ -2,10 +2,20 @@
 {
     public class LoadingService
     {
+        /// <summary>
+        /// Occurs when the loading state changes. Listeners can use this event to react to changes in the loading state, such as showing or hiding a loading indicator.
+        /// </summary>
         public event Action<bool> OnLoadingChanged;
 
         private bool _isLoading;
 
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the loading indicator should be displayed.
+        /// </summary>
+        /// <value>
+        /// <c>true</c> if the loading indicator should be displayed; otherwise, <c>false</c>.
+        /// </value>
         public bool IsLoading
         {
             get => _isLoading;
@@ -18,8 +28,14 @@
             }
         }
 
+        /// <summary>
+        /// Sets the loading state to <c>true</c>, indicating that a loading process has started and a loading indicator may be displayed.
+        /// </summary>
         public void StartLoading() => IsLoading = true;
 
+        /// <summary>
+        /// Sets the loading state to <c>false</c>, indicating that loading has completed and any loading indicators can be hidden.
+        /// </summary>
         public void StopLoading() => IsLoading = false;
     }
 }
